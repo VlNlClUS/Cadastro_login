72257ca697868b03d47bd73df80262fb0ac2168a
VlNlClUS <vinicius.cavalheiro0721@unilasalle.edu.br>
2021-02-17 09:38:44 -0300

    V_1.01

--- classes/ClassDispatch.php
+++ classes/ClassDispatch.php
@@ -5,44 +5,35 @@ use Traits\TraitParseUrl;
 
 class ClassDispatch{
 
-    private $unit;
+    private $init;
     private $url;
-    private $udir=null;
+    private $dir=null;
     private $cont;
     private $file;
     private $page;
-
 
     use TraitParseUrl;
 
     public function __construct()
     {
-
         $this->url=TraitParseUrl::parseUrl();
-        $this->count=count($this->url);
+        $this->cont=count($this->url);
         $this->verificaParametros();
-
     }
 
-
-    #Verificar se existe parametros digitados pelo usuario
-
+    #Verificar se existem parâmetros digitados pelo usuário
     private function verificaParametros()
     {
-
-        if($this->count == 1 && empty($this->url[0])){
+        if($this->cont == 1 && empty($this->url[0])){
             $this->page=DIRREQ.'views/index.php';
         }else{
-
             $this->verificaDir();
-
         }
+    }
 
-    } 
-
+    #Verificar se o índice digitado pelo usuário é um diretório
     private function verificaDir()
     {
-
         $this->init=$this->url[0].'/';
 
         for($i=0; $i<$this->cont; $i++){
@@ -52,60 +43,43 @@ class ClassDispatch{
                 }elseif(is_dir($this->dir.$this->url[$i])){
                     $this->dir.=$this->url[$i].'/';
                 }else{
-
                     $this->file=$this->url[$i];
                     break;
                 }
             }else{
-
                 if($i==0){
-                
                     $this->dir.='views/';
-
                 }
-                
+
                 if(is_dir($this->dir.$this->url[$i])){
-
                     $this->dir.=$this->url[$i].'/';
-
                 }else{
-
                     $this->file=$this->url[$i];
                     break;
                 }
-               
-
             }
-
-
         }
 
         $this->dir=str_replace("//","/",$this->dir);
         $this->verificaFile();
-
     }
 
+    #Verificar se existe o arquivo requisitado, se não existir ele chama o index.php, senão chama a pagina 404.
+    private function verificaFile()
+    {
+        $dirAbs=DIRREQ.$this->dir;
+        if(file_exists($dirAbs.$this->file.'.php')){
+            $this->page=$dirAbs.$this->file.'.php';
+        }elseif(file_exists($dirAbs.'index.php')){
+            $this->page=$dirAbs.'index.php';
+        }else{
+            $this->page=DIRREQ.'views/404.php';
+        }
+    }
 
-#Verificar se existe o arquivo requisirado, se nao existir ele chama o index.php, senão chama a pagina 404
-private function verificaFile(){
-
-$dirAbs=DIRREQ.$this->dir;
-if(file_exists($dirAbs.$this->file.'.php')){
-    $this->page-$dirAbs.$this->file.'.php';
-}elseif(file_exists($dirAbs.'index.php')){
-    $this->page=$dirAbs.'index.php';
-}else{
-    $this->page=DIRREQ.'views/404.php';
-}
-
-}
-
-#retornar a pagina final para o sistema
-public function getInclusao()
-{
-
-return $this->page;
-
-}
-
+    #Retornar a página final para o sistema
+    public function getInclusao()
+    {
+        return $this->page;
+    }
 }
